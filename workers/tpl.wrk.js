6e03ea52ca00c4daceb5b5ba6efb9c6caada93d3
Chetas Murali <me@chetasr.co>
2024-05-02 17:13:54 +0530

    init code

---
+++ workers/tpl.wrk.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const WrkBase = require('bfx-wrk-base')
+const debug = require('debug')('wrk:proc')
+
+class TplWrk extends WrkBase {
+  init () {
+    super.init()
+
+    this.setInitFacs([
+      ['fac', 'hp-svc-facs-store', 's0', 's0', {
+        storeDir: `store/${this.ctx.rack}`
+      }, -5],
+      ['fac', 'hp-svc-facs-net', 'r0', 'r0', () => {
+        return {
+          fac_store: this.store_s0
+        }
+      }, 0]
+    ])
+  }
+
+  debug (data) {
+    debug(`[THING/${this.rackId}]`, data)
+  }
+
+  getRpcKey () {
+    return this.net_r0.rpcServer.publicKey
+  }
+
+  _start (cb) {
+    async.series([
+      next => { super._start(next) },
+      async () => {
+        await this.net_r0.startRpcServer()
+        const rpcServer = this.net_r0.rpcServer
+
+        rpcServer.respond('echo', x => x)
+
+        this.status.rpcPublicKey = this.getRpcKey().toString('hex')
+
+        this.saveStatus()
+      }
+    ])
+  }
+}
+
+module.exports = TplWrk
